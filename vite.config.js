3e9e365261dea625a23de6ec682841ea709c8175
sonia <soniayjw@gmail.com>
2024-04-18 04:44:52 +0800

    update 路徑 for dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 開發中、產品路徑
+  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/' : '/',
   plugins: [react()],
 })
